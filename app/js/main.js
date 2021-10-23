ef0887afa968ea1104d3bb8dbb2696ec450909eb
ilkin140293 <ilkin-vlad@mail.ru>
2021-10-23 23:36:53 +0500

    added products list

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,35 @@
 $( function(){
 
+  $('.star').rateYo({
+    rating: 4,
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    starWidth: "12px",
+  });
+
+  $('.star-big').rateYo({
+    rating: 2,
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    starWidth: "18px",
+  });
+
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
   $('.top-slider__inner').slick({
 
     arrows: false,
--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,8 @@ function scripts() {
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/@fancyapps/ui/dist/fancybox.umd.js',
     'node_modules/mixitup/dist/mixitup.min.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
