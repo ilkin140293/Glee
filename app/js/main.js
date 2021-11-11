2cbd9eb86ae5b76be9dc2b2a5212b7e640e7b645
ilkin140293 <ilkin-vlad@mail.ru>
2021-11-11 16:59:23 +0500

    added adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
 $( function(){
 
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+    $(this).toggleClass('menu__btn--active');
+  })
+
   $('.product-page__input').styler({
 
   });
